da8bab3845ddb6944833d9931566bc1b34c77a87
RAJESH VEERANKI <rajesh@RAJESHs-MacBook-Air.local>
2025-08-09 12:18:52 +0530

    Updated Manish.py

---
+++ Manish.py
@@ -0,0 +1,12 @@
+stack = [Great Day]  # Empty stack
+
+# Add books
+stack.append("Reacher")
+stack.append("Friends")
+stack.append("Suits LA")
+
+print("Current Stack:", stack)
+
+# Remove top book
+stack.pop()
+print("After removing top book:", stack)
